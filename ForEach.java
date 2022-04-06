1e302426db3c9de6a0a87c30220eb47be38975ce
sanguinesiva <sivakandaswamy@outlook.com>
2022-04-06 06:12:23 -0500

    new java code

---
+++ ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String [] names= {"siva", "ragu", "vimal"};
+        for (String name:names)
+            System.out.println(name);
+            
+    }
+}
---
+++ WhileLoop.java
@@ -0,0 +1,20 @@
+public class WhileLoop{
+    public static void main(String[] args) {
+        // int num= 20;
+        // while (num>=15) {
+        //     System.out.println(num);
+        //     num--;
+        int i= 10;
+        while (i<=15) {
+            if (i%2==0) {
+                System.out.println(i);
+                
+            }
+            i++;
+            
+        }
+
+            
+    }
+ }
+
